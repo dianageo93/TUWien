8c0bd559c503038488ebba54b3fba9412f6a1836
Georgiana <dianageo93@yahoo.com>
2014-12-02 14:33:49 +0100

    Prettifying the code.

--- Parallel Computing/src/bucket_sort_seq.cpp
+++ Parallel Computing/src/bucket_sort_seq.cpp
@@ -1,29 +1,42 @@
+#include <algorithm>
 #include <cstdlib>
 #include <vector>
-#include <algorithm>
+
 #include "bucket_sort_seq.h"
 
-using namespace std;
 using std::vector;
 using std::sort;
 
-void bucketSort (int* vector, int size, int range, int num_of_buckets) {
-    std::vector<std::vector<int> > buckets(num_of_buckets, std::vector<int>());
+int getMinVecotr (int* vector, int size) {
     int min = vector[0];
-    int max = vector[0];
     for (int i = 0; i < size; i++) {
         min = vector[i] < min ? vector[i] : min;
+    }
+    return min;
+}
+
+int getMaxVecotr (int* vector, int size) {
+    int max = vector[0];
+    for (int i = 0; i < size; i++) {
         max = vector[i] > max ? vector[i] : max;
     }
+    return max;
+}
+
+void bucketSort (int* vector, int size, int range, int num_of_buckets) {
+    std::vector<std::vector<int> > buckets(num_of_buckets, std::vector<int>());
+    int min = getMinVecotr (vector, size);
+    int max = getMaxVecotr (vector, size);
     float delta = max - min;
 
-    // place values in buckets
+    // Place values in their buckets.
     for (int i = 0; i < size; i++) {
-        int bucket = (vector[i] - min)/delta * (num_of_buckets - 1);
-        buckets[bucket].push_back(vector[i]);
+        int bucket = (vector[i] - min) / delta * (num_of_buckets - 1);
+        buckets[bucket].push_back (vector[i]);
     }
+
+    // Sort buckets and replace the values in the initial vector.
     int k = 0;
-    // sort buckets and place the values in the initial vector
     for (int i = 0; i < num_of_buckets; i++) {
         if (buckets[i].size() > 0) {
             sort(buckets[i].begin(), buckets[i].end());
