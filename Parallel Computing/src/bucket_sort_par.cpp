a5e1b5fb1f9cc0386cb381e7aa53abb08292f3b6
Mihai Varga <mihai.mv13@gmail.com>
2014-12-04 21:19:36 +0100

    task pool synchronization

--- Parallel Computing/src/bucket_sort_par.cpp
+++ Parallel Computing/src/bucket_sort_par.cpp
@@ -30,25 +30,27 @@ struct work_t {
 };
 
 struct tpool_t {
-    int num_threads, max_size, curr_size;
+    int num_threads, max_size, curr_size, num_tasks;
     pthread_t *threads;
     work_t *head, *tail;
     pthread_mutex_t lock;
-    pthread_cond_t not_empty, not_full;
+    pthread_cond_t not_empty, not_full, done;
 };
 
 void* tpool_thread (void* arg);
 
-tpool_t* pool_init (int num_threads, int max_size) {
+tpool_t* pool_init (int num_threads, int max_size, int num_tasks) {
     tpool_t *tpl = new tpool_t;
     tpl->num_threads = num_threads;
     tpl->max_size = max_size;
+    tpl->num_tasks = num_tasks;
     tpl->curr_size = 0;
     tpl->head = tpl->tail = NULL;
 
     pthread_mutex_init (&(tpl->lock), NULL);
     pthread_cond_init (&(tpl->not_empty), NULL);
     pthread_cond_init (&(tpl->not_full), NULL);
+    pthread_cond_init (&(tpl->done), NULL);
     tpl->threads = new pthread_t[num_threads];
     for (int i = 0; i < num_threads; i++) {
         pthread_create (&(tpl->threads[i]), NULL, tpool_thread, (void*) tpl);
@@ -77,7 +79,14 @@ void* tpool_thread (void* arg) {
             pthread_cond_broadcast (&(tpl->not_full));
         }
         pthread_mutex_unlock (&(tpl->lock));
+
         (*(work->routine))(work->arg);
+        pthread_mutex_lock (&(tpl->lock));
+        tpl->num_tasks--;
+        if (tpl->num_tasks == 0) {
+            pthread_cond_signal (&(tpl->done));
+        }
+        pthread_mutex_unlock (&(tpl->lock));
         delete work;
     }
     return NULL;
@@ -139,18 +148,19 @@ void bucketSort_par (int* vector, int size, int range, int num_of_buckets) {
     int num_threads = 1;
     int block = size / num_threads;
 
-    tpool_t *pool = pool_init (num_threads, 100000);
+    tpool_t *pool = pool_init (num_threads, 100000, num_threads);
 
     for (int i = 0; i < num_threads; i++) {
         partition_t *p = new partition_t(vector, i * block, (i+1) * block, min,
                 delta, bucketContainer);
         tpool_insert(pool, partition, (void*)p);
     }
+    // TODO size % num_threads != 0
 
-    sleep(1);
-    //for (int i = 0; i < num_threads; i++) {
-    //    pthread_join (pool->threads[i], NULL);
-    //}
+    pthread_mutex_lock (&(pool->lock));
+    while (pool->num_tasks > 0) {
+        pthread_cond_wait (&(pool->done), &(pool->lock));
+    }
 
     for (int i = 0; i < num_of_buckets; i++) {
         if (bucketContainer->buckets[i].size() > 0) {
