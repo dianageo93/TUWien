13bfb89027114c78c3d49591480b039f727bbdce
Mihai Varga <mihai.mv13@gmail.com>
2014-12-15 13:04:39 +0100

    Sort method

--- Parallel Computing/src/test_sort.cpp
+++ Parallel Computing/src/test_sort.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
 bool test_is_sorted_asc (int* vector, int size) {
     for (int i = 0; i < size - 1; i++) {
         if (vector[i] > vector[i + 1]) {
@@ -14,4 +18,11 @@ bool test_is_sorted_desc (int* vector, int size) {
         }
     }
     return true;
+}
+
+void print (int* vector, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << vector[i] << " ";
+    }
+    cout << endl;
 }
