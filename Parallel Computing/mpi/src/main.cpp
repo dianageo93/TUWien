89196818807d92e23816b0914ebc66aaa95911db
Mihai Varga <mihai.mv13@gmail.com>
2014-12-28 16:08:47 +0100

    MPI added times

--- Parallel Computing/mpi/src/main.cpp
+++ Parallel Computing/mpi/src/main.cpp
@@ -31,31 +31,41 @@ int main (int argc, char** argv) {
     int num_of_elems = atoi (argv[1]); 
     int range = atoi (argv[2]);
 
+    double vec_gen_time;
     int *vector, *copy;
     if (rank == 0) {
+        cout << "Start vector generating\n";
+        double start = MPI_Wtime();
         vector = generate_random_vector (num_of_elems, range);
         copy = new int[num_of_elems];
         for (int i = 0; i < num_of_elems; i++) {
             copy[i] = vector[i];
         }
+        double end = MPI_Wtime();
+        vec_gen_time = end -start;
+        cout << "Done vector generating: " << end - start << endl;
     }
 
-    count::countSort_par (copy, num_of_elems, range);
     if (rank == 0) {
-        countSort_seq (vector, num_of_elems, range);
+        cout << "Start parallel sort\n";
+    }
+    double start = MPI_Wtime();
+    count::countSort_par (copy, num_of_elems, range);
+    double end = MPI_Wtime();
+    double elapsed = end - start;
+    double max_time;
+    MPI_Reduce (&elapsed, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        cout << "Parallel time: " << end - start << endl;
+        cout << "Parallel max time: " << max_time - vec_gen_time << endl;
     }
 
-    struct timespec begin, end;
-    double elapsed;
-
-    //clock_gettime (CLOCK_MONOTONIC, &begin);
-    //cout << "Sequential sort\n";
-    //countSort_seq(vector, num_of_elems, range);
-    //clock_gettime (CLOCK_MONOTONIC, &end);
-    //elapsed = end.tv_sec - begin.tv_sec;
-    //elapsed += (end.tv_nsec - begin.tv_nsec) / 1000000000.0;
-    //cout << elapsed << endl;
-
+    if (rank == 0) {
+        double start_seq = MPI_Wtime();
+        countSort_seq (vector, num_of_elems, range);
+        double end_seq = MPI_Wtime();
+        cout << "Sequential time: " << end_seq - start_seq << endl;
+    }
 
     if (rank == 0) {
         if (test_is_sorted_asc (copy, num_of_elems)) {
